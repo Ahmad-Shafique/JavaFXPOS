aa13d7e4d1e30c5c2b1e8e0cc071a74fb5ff63a4
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2018-03-10 23:03:57 +0600

    Added category add and edit UI

--- src/ui/controllers/category/AddController.java
+++ src/ui/controllers/category/AddController.java
@@ -1,4 +1,98 @@
 package ui.controllers.category;
 
-public class AddController {
+import core.application.services.Activator;
+import core.domain.model.entities.Category;
+import core.domain.services.interfaces.ICRUD;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddController implements Initializable {
+
+    private ObservableList<Category> CATEGORYLIST = FXCollections.observableArrayList();
+
+    @FXML
+    private TextField typeField;
+    @FXML
+    private TextArea descriptionArea;
+    @FXML
+    private Button saveButton;
+    private ICRUD<Category> service;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        service = (ICRUD<Category>) new Activator().activate("Category");
+    }
+
+    @FXML
+    public void handleCancel(ActionEvent event) {
+        typeField.setText("");
+        descriptionArea.setText("");
+    }
+
+    @FXML
+    public void handleSave(ActionEvent event) {
+
+        if (validateInput()) {
+
+            Category category = new Category(
+                    -1,
+                    typeField.getText(),
+                    descriptionArea.getText()
+            );
+
+            service.create(category);
+            CATEGORYLIST.clear();
+            CATEGORYLIST.addAll(service.getAll());
+
+            ((Stage) saveButton.getScene().getWindow()).close();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Successful");
+            alert.setHeaderText("Category Created!");
+            alert.setContentText("Category is created successfully");
+            alert.showAndWait();
+        }
+    }
+
+    private boolean validateInput() {
+
+        String errorMessage = "";
+
+        if (typeField.getText() == null || typeField.getText().length() == 0) {
+            errorMessage += "No valid name!\n";
+        }
+
+        if (descriptionArea.getText() == null || descriptionArea.getText().length() == 0) {
+            errorMessage += "No email description!\n";
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+
+    @FXML
+    public void closeAction(ActionEvent event) {
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
 }
--- src/ui/controllers/category/EditController.java
+++ src/ui/controllers/category/EditController.java
@@ -1,4 +1,115 @@
 package ui.controllers.category;
 
-public class EditController {
+import core.application.services.Activator;
+import core.domain.model.entities.Category;
+import core.domain.services.interfaces.ICRUD;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditController implements Initializable{
+
+    private ICRUD<Category> service;
+    private Category category;
+    private long selectedCategoryId;
+    private ObservableList<Category> CATEGORYLIST = FXCollections.observableArrayList();
+
+    @FXML
+    private TextField typeField;
+    @FXML
+    private TextArea descriptionArea;
+    @FXML
+    private Button saveButton;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        service = (ICRUD<Category>) new Activator().activate("Category");
+        resetValues();
+    }
+
+    public void setCategory(Category category, long selectedCategoryId) {
+        this.category = category;
+        this.selectedCategoryId = selectedCategoryId;
+        setData();
+    }
+
+    @FXML
+    public void handleSave(ActionEvent event) {
+
+        if (validateInput()) {
+
+            Category editedCategory = new Category(
+                    category.getId(),
+                    typeField.getText(),
+                    descriptionArea.getText()
+            );
+
+            service.update(editedCategory.getId(),editedCategory);
+            CATEGORYLIST.set((int) selectedCategoryId, editedCategory);
+
+            ((Stage) saveButton.getScene().getWindow()).close();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Successful");
+            alert.setHeaderText("Category Updated!");
+            alert.setContentText("Category is updated successfully");
+            alert.showAndWait();
+        }
+    }
+
+    private void setData() {
+        typeField.setText(category.getName());
+        descriptionArea.setText(category.getDescription());
+    }
+
+    private void resetValues() {
+        typeField.setText("");
+        descriptionArea.setText("");
+    }
+
+    @FXML
+    public void handleCancel(ActionEvent event) {
+        resetValues();
+    }
+
+    private boolean validateInput() {
+
+        String errorMessage = "";
+
+        if (typeField.getText() == null || typeField.getText().length() == 0) {
+            errorMessage += "No valid name!\n";
+        }
+
+        if (descriptionArea.getText() == null || descriptionArea.getText().length() == 0) {
+            errorMessage += "No email description!\n";
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+
+    @FXML
+    public void closeAction(ActionEvent event) {
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
 }
